8b39f97e718adc8f0ebd5aca5413c3b9f16bf885
mowafy001 <wafy123445@gmail.com>
2022-07-08 13:24:25 +0200

    add some options

--- script.py
+++ script.py
@@ -1,5 +1,18 @@
 import pdfkit
 
-css = 'style.css'
-pdfkit.from_file('layout.html', 'out.pdf', css=css)
+options = {
+    'page-size': 'Letter',
+    'margin-top': '0',
+    'margin-right': '0',
+    'margin-bottom': '0',
+    'margin-left': '0',
+    'encoding': "UTF-8",
+    'custom-header': [
+        ('Accept-Encoding', 'gzip')
+    ],
+    'no-outline': None
+}
+
+css = [ 'css/style.css' ]
+pdfkit.from_file('layout.html', 'out.pdf', options=options, css=css)
 
