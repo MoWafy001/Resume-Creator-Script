16f47979486d2475542e7a139e94ffac61f2c36c
mowafy001 <wafy123445@gmail.com>
2022-07-08 14:19:30 +0200

    load css automatically

--- src/pdf_gen.py
+++ src/pdf_gen.py
@@ -1,4 +1,5 @@
 import pdfkit
+import os
 
 options = {
     'page-size': 'Letter',
@@ -13,7 +14,9 @@ options = {
     'no-outline': None
 }
 
-css = [ 'css/style.css' ]
+# load the css file names automaticlly from the css directory
+dir_name = 'css'
+css = list(map(lambda x: f"{dir_name}/{x}" , os.listdir(dir_name)))
 
 def create_pdf(layout):
     pdfkit.from_string(layout, 'out.pdf', options=options, css=css)
