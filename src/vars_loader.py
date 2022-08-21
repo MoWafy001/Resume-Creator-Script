5c45b8ec7bc06dfc6b37ee03914db6c0bdb5887d
mowafy001 <wafy123445@gmail.com>
2022-08-21 02:42:37 +0200

    Cleaning vars_loader.py a lil bit

--- src/vars_loader.py
+++ src/vars_loader.py
@@ -1,9 +1,15 @@
 import yaml
 
+
 class VarsReader:
     def __init__(self, vars_path='vars.yaml'):
+        self.open_yaml(vars_path)
+
+
+    def open_yaml(self, vars_path):
         with open(vars_path) as f:
             self.vars_d = yaml.safe_load(f)
+
 
     def get_var(self, var_name):
         if var_name not in self.vars_d:
