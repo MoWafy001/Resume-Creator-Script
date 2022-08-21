33be63f0844e69ecc912bdd6e637a56cfca3122f
mowafy001 <wafy123445@gmail.com>
2022-08-21 04:15:28 +0200

    Break things up

--- src/HTMLParser.py
+++ src/HTMLParser.py
@@ -33,43 +33,26 @@ class HTMLParser:
         blocks = line.split(BLOCK_SIMBOL)[1::2]
 
         for block in blocks:
-            block_l = block.split('?')
-            block_name = block_l[0].strip()
-            dep = None
+            block_name, dep = self.get_block_name_and_dep(block)
 
-            if len(block_l) == 2:
-                dep = block_l[1]
-                if dep.strip() == '':
-                    dep = block_name
-
-            
-            block_html = ''
-            if dep is not None:
-                try:
-                    self.get_var(dep)
-                    block_html = self.load_html(block_name)  
-                except:
-                    block_html = ''
-            else:
-                block_html = self.load_html(block_name)  
-
-
+            block_html = self.get_block_html(block_name, dep)
             block_html = self.parse_html(block_html)
-            line = line.replace(
-                f"{BLOCK_SIMBOL}{block}{BLOCK_SIMBOL}", block_html)
+
+            line = self.symbol_to_value(line, BLOCK_SIMBOL,
+                                            block, block_html)
 
         return line
 
 
-    # find values in a line and replace
+    # find variables in lines and replace
     def handel_variables(self, line):
         vars_found = line.split(VAR_SIMBOL)[1::2]
 
         for var_name in vars_found:
             var_val = self.get_var(var_name)
 
-            line = line.replace(
-                f"{VAR_SIMBOL}{var_name}{VAR_SIMBOL}", self.get_var(var_name))
+            line = self.symbol_to_value(line, VAR_SIMBOL,
+                                            var_name, self.get_var(var_name))
 
         return line
 
@@ -93,11 +76,7 @@ class HTMLParser:
                 pieces[i] = ''
             else:
                 b = v
-                try:
-                    listordict = self.get_var(key)
-                    out = self.loop_over_list_or_dict(listordict, b, key)
-                except:
-                    out = ''
+                out = self.list_dict_get_out(b, key)
                 pieces[i] = out
                 skip = True
                 key = None
@@ -108,23 +87,84 @@ class HTMLParser:
     def loop_over_list_or_dict(self, var, b, key):
         out = ""
         if type(var) is list:
-            for val in var:
-                if type(val) is str:
-                    nb = b.replace(f"{LIST_DICT_SIMBOL_VALUE}{key}_val{LIST_DICT_SIMBOL_VALUE}", val.replace('\n', '<br>'))
-                    out += nb
-                else:
-                    out += self.loop_over_list_or_dict(val, b, key)
+            out = self.handel_list(out, var, b, key)
         else: # dict
-            for k in var:
-                nb = b.replace(f"{LIST_DICT_SIMBOL_VALUE}{key}_key{LIST_DICT_SIMBOL_VALUE}", k.replace('\n', '<br>'))
-                if type(var[k]) == list:
-                    c = 0
-                    for i in var[k]:
-                        nb = nb.replace(f"{LIST_DICT_SIMBOL_VALUE}{key}_val_{c}{LIST_DICT_SIMBOL_VALUE}", i.replace('\n', '<br>'))
-                        c += 1
-                    out += nb
-                else:
-                    nb = nb.replace(f"{LIST_DICT_SIMBOL_VALUE}{key}_val{LIST_DICT_SIMBOL_VALUE}", var[k].replace('\n', '<br>'))
-                    out += nb
+            out = self.handel_dict(out, var, b, key)
 
         return out
+
+    
+    """
+    Helper Methods
+    """
+
+    def get_block_name_and_dep(self, block):
+        block_l = block.split('?')
+        block_name = block_l[0].strip()
+        dep = None
+
+        if len(block_l) == 2:
+            dep = block_l[1]
+            if dep.strip() == '':
+                dep = block_name
+
+        return block_name, dep
+
+
+    def get_block_html(self, block_name, dep):
+        if dep is not None:
+            try:
+                self.get_var(dep)
+                return self.load_html(block_name)  
+            except:
+                return ''
+        
+        return self.load_html(block_name)
+
+
+    def symbol_to_value(self, line, symbol, var, value):
+        return line.replace(f"{symbol}{var}{symbol}", value)
+
+
+    def list_dict_get_out(self, b, key):
+        try:
+            listordict = self.get_var(key)
+            return self.loop_over_list_or_dict(listordict, b, key)
+        except:
+            return ''
+
+
+    def handel_list(self, out, var, b, key):
+        for val in var:
+            if type(val) is str:
+                nb = self.symbol_to_value(b, LIST_DICT_SIMBOL_VALUE,
+                                            key+"_val", val.replace('\n', '<br>'))
+                out += nb
+            else:
+                out += self.loop_over_list_or_dict(val, b, key)
+        return out
+
+
+    def handel_dict(self, out, var, b, key):
+        for k in var:
+            nb = self.symbol_to_value(b, LIST_DICT_SIMBOL_VALUE,
+                                        key+"_key", k.replace('\n', '<br>'))
+            if type(var[k]) == list:
+                out += self.handel_list_in_dict(var[k], nb, key)
+
+            else:
+                nb = self.symbol_to_value(nb, LIST_DICT_SIMBOL_VALUE,
+                                            f"{key}_val", var[k].replace('\n', '<br>'))
+                out += nb
+
+        return out
+
+
+    def handel_list_in_dict(self, var_k, nb, key):
+        c = 0
+        for i in var_k:
+            nb = self.symbol_to_value(nb, LIST_DICT_SIMBOL_VALUE,
+                                        f"{key}_val_{c}", i.replace('\n', '<br>'))
+            c += 1
+
+        return nb
