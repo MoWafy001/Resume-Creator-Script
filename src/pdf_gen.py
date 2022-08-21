570548407aa72575dcafc254481930b9e3c6fd7f
mowafy001 <wafy123445@gmail.com>
2022-08-21 02:44:45 +0200

    Cleaning pdf_gen.py a lil bit

--- src/pdf_gen.py
+++ src/pdf_gen.py
@@ -1,6 +1,7 @@
 import pdfkit
 import os
 
+
 options = {
     'encoding': "UTF-8",
     'custom-header': [
@@ -12,11 +13,16 @@ options = {
 
 def create_pdf(layout, file_name, template, script_dir):
 
+
     # load the css file names automaticlly from the css directory
     dir_name = f'{script_dir}/templates/{template}/css'
     css = list(map(lambda x: f"{dir_name}/{x}" , os.listdir(dir_name)))
 
+
+    # Creates out/ if doesn't exist    
     if 'out' not in os.listdir(script_dir):
         os.mkdir(f'{script_dir}/out')
 
+
+    # create the PDF resume
     pdfkit.from_string(layout, f'{script_dir}/out/{file_name}.pdf', options=options, css=css)
